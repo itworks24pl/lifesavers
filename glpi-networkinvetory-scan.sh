a1aeb26b21f5380591cda99f8b57405308213551
Paweł Sadowski <40364774+psad73@users.noreply.github.com>
2025-04-05 22:46:12 +0200

    Create glpi-networkinvetory-scan.sh

---
+++ glpi-networkinvetory-scan.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+netscan () {
+    echo $1
+    # glpi-netdiscovery --first ${1}.0 --last ${1}.254 --entity EFFERTA --threads 5 -i -s net-${1}.0 --debug
+    glpi-netdiscovery --first $1.0 --last $1.254 --entity ENTITY --threads 5 -i -s net-$1.0 --debug
+}
+
+netscan 10.10.0
+netscan 10.10.1
+netscan 10.10.2
