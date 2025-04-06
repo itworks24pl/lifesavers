cf72c82b1b65c0c638eab145074f63f895931fad
Paweł Sadowski <40364774+psad73@users.noreply.github.com>
2025-04-06 11:18:01 +0200

    Update glpi-networkinvetory-scan.sh

--- glpi-networkinvetory-scan.sh
+++ glpi-networkinvetory-scan.sh
@@ -2,6 +2,7 @@
 netscan () {
     echo $1
     glpi-netdiscovery --first $1.0 --last $1.254 --entity ENTITY --threads 5 -i -s net-$1.0 --debug
+    glpi-injector -R -v -x -u http://10.10.11.2:22571/front/inventory.php -d net-$1.0
 }
 
 netscan 10.10.0
